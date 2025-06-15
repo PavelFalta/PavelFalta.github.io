0632ea29bb776229a9598bcd7c8bb342bc168eb1
PavelFalta <paja.falta@gmail.com>
2025-06-15 18:13:57 +0200

    bmg

--- docs/bmg/js/config.js
+++ docs/bmg/js/config.js
@@ -3,7 +3,7 @@
 
 const CONFIG = {
     // Backend server configuration
-    BACKEND_URL: 'http://localhost:8000',
+    BACKEND_URL: 'http://bmg-production.up.railway.app:8000',
     
     // API endpoints (automatically constructed from BACKEND_URL)
     get API_HEALTHCHECK() {
