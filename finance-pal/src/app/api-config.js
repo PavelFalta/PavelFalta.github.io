63717ff3239063af17df1787d1ce9c05dc2a3721
PavelFalta <paja.falta@gmail.com>
2025-09-06 00:59:36 +0200

    lol

--- finance-pal/src/app/api-config.js
+++ finance-pal/src/app/api-config.js
@@ -1,4 +1,4 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://finance-backend-production-f25f.up.railway.app";
+export const API_URL = "https://finance-backend-production-f25f.up.railway.app";
 
 
 
